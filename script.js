6cf46aa55a2189aacb0c0b336682e18101a24ac5
jacopolegovini <legovini96@gmail.com>
2024-07-31 15:15:47 +0200

    data elaboration completed

--- script.js
+++ script.js
@@ -6,26 +6,35 @@ console.log('OK')
 3. Raccogliere i dati
     - Richiesta numero chilometri
     - Richiesta età passeggero
-4. Controllo dati
-    - Controllare età
-5. Elaborazione dati
+4. Elaborazione dati
     - Prezzo al chilometro per chilometri dichiarati
     - Riduzione in base all'età
-6. Produzione output
+5. Produzione output
 */
 
 // 1. Collegarsi al DOM e recuperare gli elementi
-const totalPrice = document.getElementById('price');
+let totalPrice = document.getElementById('price');
 console.log (totalPrice);
 
 // 2. Dichiarare le variabili
 const pricePerKM = 0.21;
-const youngDiscount = 0.21 - (0.21 * 20 / 100);
-const seniorDiscount = 0.21 - (0.21 * 40 / 100);
+const youngDiscount = pricePerKM - (0.21 * 20 / 100);
+const seniorDiscount = pricePerKM - (0.21 * 40 / 100);
 console.log (seniorDiscount);
 
 // 3. Raccogliere i dati
-const KMNumber = prompt('Quanti chilometri vorresti fare?', '50');
+const KMNumber = prompt('Quanti chilometri vorresti fare?', '10');
 const userAge = prompt('Quanti anni hai?', '28');
-console.log ('KMNumber:', KMNumber)
-console.log ('userAge:', userAge)
+console.log ('KMNumber:', KMNumber);
+console.log ('userAge:', userAge);
+
+// 4. Elaborazione dati
+totalPrice = pricePerKM * KMNumber;
+console.log(totalPrice);
+
+if (userAge <= 18) {
+    totalPrice = youngDiscount * KMNumber
+} else if (userAge >= 65) {
+    totalPrice = seniorDiscount * KMNumber
+}
+console.log(totalPrice);
